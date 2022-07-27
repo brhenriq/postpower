235e809504cc6f17bc1f872a0197884d7fc0d56c
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2022-07-27 19:34:54 -0300

    ADD: Create and List Users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CreateUserDto } from './user.dto';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+@UseInterceptors(ClassSerializerInterceptor)
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() body: CreateUserDto): Promise<User> {
+    return this.userService.create(body);
+  }
+
+  @Get()
+  async listAll(): Promise<User[]> {
+    return this.userService.listAll();
+  }
+}
